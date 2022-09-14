9b71d1c08eddbf58857fe979f089eade5e6e3fa4
imroc <dpl1024@163.com>
2022-09-15 01:48:24 +0800

    自己创建commander实例

--- commander-test/bin/index.js
+++ commander-test/bin/index.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
+console.log('commander-test')
+
 const pkg = require('../package.json')
 const commander = require('commander');
-const {program} = commander
+
+// const {program} = commander
+const program = new commander.Command()
 
 program
+  .name(Object.keys(pkg.bin)[0])
+  .usage('<command> [options]')
   .version(pkg.version)
-  .parse(process.argv)
+  .option('-d, --debug', '是否开启调试模式', false)
+  .option('-e, --envName <envName>', '获取环境变量名称')
+  .parse(process.argv);
+
+const options = program.opts()
+console.log(options);
+program.outputHelp()
