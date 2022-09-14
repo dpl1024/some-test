dde074c65f7017bb1345e462314ecbb4c988c38b
imroc <dpl1024@163.com>
2022-09-15 01:59:12 +0800

    命令注册

--- commander-test/bin/index.js
+++ commander-test/bin/index.js
@@ -14,7 +14,34 @@ program
   .version(pkg.version)
   .option('-d, --debug', '是否开启调试模式', false)
   .option('-e, --envName <envName>', '获取环境变量名称')
-  .parse(process.argv);
+
+const clone = program.command('clone <source> [destination]');
+clone
+  .description('clone a repository')
+  .action((source, destination, cmdObj) => {
+    console.log('do clone', source, destination)
+    console.log('cmdObj > ', cmdObj);
+  })
+  .option('-f, --force', '是否强制克隆')
+  .option('-n, --name <name>', '是否强制克隆')
+
+const service = new commander.Command('service')
+service
+  .command('start [port]')
+  .description('start service at the port')
+  .action((port) => {
+    console.log('do service start', port)
+  })
+service
+  .command('stop')
+  .description('stop service')
+  .action(() => {
+    console.log('stop service')
+  })
+program.addCommand(service)
+
+program.parse(process.argv);
+
 
 const options = program.opts()
 console.log(options);
